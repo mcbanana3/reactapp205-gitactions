11c95189441c160236c3125f14ce8d2c53c430dc
Aryan Y <2300030049@kluniversity.in>
2025-07-29 12:37:37 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h1>Welcome to My React Project - S205</h1>
       <p>CI/CD DEVOPS</p>
       <p>This is a simple homepage deployed on GitHub Actions using Vite.</p>
+      <p>Done with Git Pages & Git Actions</p>
     </div>
   );
 }
